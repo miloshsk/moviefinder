e9f0f53976d9d37a12964bf2fbd8293e455bf807
miloshsk <miloshsk@gmail.com>
2018-08-22 00:03:04 +0400

    Add sort

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {	
 	let movieList = document.querySelector('.movies-list');
 	let form = document.querySelector('.movies-search');
+	let sortItems = document.querySelectorAll('.movie-sort');
 
 	function addMovieToList(movie) {
 		let item = document.createElement('li');
 		item.classList.add('movies-item');
+		item.dataset.title = `${movie.Title.toLowerCase()}`;
+		item.dataset.year = `${movie.Year.substr(0,4)}`;
 		movieList.appendChild(item);
 
 		let imgWrapper = document.createElement('div');
@@ -45,7 +48,6 @@ document.addEventListener('DOMContentLoaded', function() {
 		errorTitle.appendChild(document.createTextNode('Nothing found'));
 		movieList.appendChild(errorTitle);
 	}
-
 	function getData(url) {
 		return new Promise(function(resolve, reject) {
 			let xhr = new  XMLHttpRequest();
@@ -66,6 +68,9 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 	form.addEventListener('submit',function(e) {
 		e.preventDefault();
+		for(var i = 0; i < sortItems.length; i++) {
+			sortItems[i].checked = false;
+		}
 		let input = document.querySelector('.movies-search-field');
 		getData(`http://www.omdbapi.com/?apikey=e99e23f5&s=${input.value}`)
 			.then(function(movies) { 
@@ -74,7 +79,26 @@ document.addEventListener('DOMContentLoaded', function() {
 					)})
 			.then(clearList(movieList))
 			.catch(showError);
-	})
+	});
+
+	for(var i = 0; i < sortItems.length; i++) {
+		sortItems[i].addEventListener('change', function() {
+			var movies = document.querySelectorAll('.movies-item');
+			if(this.id === 'sort-name') {
+				Array.from(movies).sort(function(a,b) {
+					return (a.dataset.title > b.dataset.title) - (a.dataset.title < b.dataset.title)
+				}).forEach(function(elem, i){
+					elem.style.order = i;
+				})
+			} else if(this.id === 'sort-year') {
+				Array.from(movies).sort(function(a,b) {
+					return a.dataset.year - b.dataset.year
+				}).forEach(function(elem, i) {
+					elem.style.order = i;
+				})
+			}
+		})
+	}
 });
 
 
